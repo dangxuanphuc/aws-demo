dd171ab95f77df1a9e62c1e877c654cc86852ea4
Phuc Dang Xuan <dangxuanphuc2211@gmail.com>
2025-05-02 11:56:12 +0900

    Update buildspec.yml

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Express.js auto-deployed from GitHub to EC2 via CodeBuild!</h1><h2>PhucDX test</h2>');
+  res.send('<h1>Express.js auto-deployed from GitHub to EC2 via CodeBuild!</h1>');
 });
 
 app.listen(port, () => {
